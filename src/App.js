1d268dfa19ba7587439e965dc5f2819c6ed1285b
Kaspar Poland <kaspar78@mouco.com>
2020-12-18 14:33:24 -0700

    Commit #1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,146 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Button from "@material-ui/core/Button";
+import "./App.css";
 
-function App() {
+const shouldWrapChild = (id) => {
+  return id === "wrap-me";
+};
+
+const wrapType = (id) => {
+  switch (id) {
+    case "wrap-me":
+      return "div";
+    default:
+      return "div";
+  }
+};
+
+const col1 = "#eb4034";
+const col2 = "#FFFFFF";
+
+const toggleColor = (event) => {
+  const to255 = (hex) => parseInt(hex, 16);
+  const to255All = (hex) =>
+    `rgb(${to255(hex.substring(1, 3))}, ${to255(hex.substring(3, 5))}, ${to255(
+      hex.substring(5)
+    )})`;
+
+  if (event.target.style.color == to255All(col2) || !event.target.style.color) {
+    event.target.style.color = col1;
+  } else {
+    event.target.style.color = col2;
+  }
+};
+
+const wrapProps = (id) => {
+  let props = {};
+  switch (id) {
+    case "wrap-me":
+      props = {
+        style: { color: col2 },
+        onMouseOver: toggleColor,
+        onMouseOut: toggleColor,
+      };
+      break;
+    default:
+      break;
+  }
+
+  return { ...props, id };
+};
+
+const RecursiveWrapper = (props) => {
+  console.log(props.children);
+
+  const wrappedChildren = React.Children.map(props.children, (child) => {
+    console.log(child.props);
+    if (child.props && child.props.children) {
+      console.log(child.props);
+      if (shouldWrapChild(child.props.id)) {
+        return React.createElement(
+          wrapType(child.props.id),
+          wrapProps(child.props.id),
+          React.cloneElement(
+            child,
+            {},
+            <RecursiveWrapper>{child.props.children}</RecursiveWrapper>
+          )
+        );
+      } else {
+        return React.cloneElement(
+          child,
+          {},
+          <RecursiveWrapper>{child.props.children}</RecursiveWrapper>
+        );
+      }
+    }
+
+    return child;
+  });
+  return <React.Fragment>{wrappedChildren}</React.Fragment>;
+};
+
+function Viewer() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+        <Button
+          style={{ backgroundColor: "white" }}
+          // onClick={(e) => setEditable(!editable)}
         >
-          Learn React
-        </a>
+          Make {true ? "static" : "responsive"}
+        </Button>
+        <div
+          id="outer"
+          style={{
+            border: `solid white 2px`,
+            borderRadius: "10px",
+            margin: "30px",
+            padding: "20px",
+          }}
+        >
+          <p id="wrap-me">Hello, world!</p>
+        </div>
       </header>
     </div>
   );
+}
+
+function App() {
+  const [editable, setEditable] = useState(true);
+  const [counter, setCounter] = useState(0);
+
+  console.log("render");
+
+  let toRender = (
+    <div className="App">
+      <header className="App-header">
+        <Button
+          style={{ backgroundColor: "white" }}
+          // onClick={(e) => setEditable(!editable)}
+        >
+          Make {true ? "static" : "responsive"}
+        </Button>
+        <div
+          id="outer"
+          style={{
+            border: `solid white 2px`,
+            borderRadius: "10px",
+            margin: "30px",
+            padding: "20px",
+          }}
+        >
+          <p id="wrap-me">Hello, world!</p>
+        </div>
+      </header>
+    </div>
+  );
+
+  if (editable) {
+    return <RecursiveWrapper>{toRender}</RecursiveWrapper>;
+  } else {
+    return toRender;
+  }
 }
 
 export default App;
